92b3ca7fb0f23238c1dd58523959562d800d0ed8
Sebastian Simko <contact@sebastiansimko.sk>
2020-12-26 13:04:29 +0100

    update logic based on previous base api

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,15 +9,21 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function list(PageCategory $category) {
-        return response()->json($category->pages);
+        $pages = $category->pages->where('page_id', null)->flatten();
+        return response()->json($pages);
     }
 
     public function detail(PageCategory $category, Page $page) {
 
-        if($category->id != $page->category->id) {
+        if($category->id != $page->page_category->id) {
             return response()->json([], 404);
         }
 
-        return response()->json($page);
+        $pageArray = $page->toArray();
+
+        $pageArray["children"] = $page->children();
+        $pageArray["parent"] = $page->parent();
+
+        return response()->json($pageArray);
     }
 }
--- app/Models/Page.php
+++ app/Models/Page.php
@@ -8,10 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
     use HasFactory;
+    protected $with = ["page_category"];
 
-
-    public function category() {
+    public function page_category() {
         return $this->belongsTo(PageCategory::class, 'page_category_id');
+    }
+
+    public function children() {
+        return Page::where('page_id', $this->id)->get("*");
+    }
+
+    public function parent() {
+        if ($this->page_id) {
+            return Page::where('id', $this->page_id)->get();
+        }
+
+        return $this;
     }
 
     public function getRouteKeyName()
