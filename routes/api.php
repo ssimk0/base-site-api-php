49f96ea1c1f88258d4113bf4435e49d4fce2bac1
Sebastián Simko <contact@sebastiansimko.sk>
2021-09-28 14:23:46 +0200

    add endpoint for send contact mail

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,14 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\UploadCategoryController;
 use App\Http\Controllers\UploadController;
 use App\Http\Controllers\UploadTypeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([
     'auth:api'
 ])->group(function ($router) {
-    Route::middleware(["can:editor,all"])->group(function() {
+    Route::middleware(["can:editor,all"])->group(function () {
         Route::post("v1/articles", [ArticleCategoryController::class, 'create']);
         Route::put("v1/articles/{category}", [ArticleCategoryController::class, 'update']);
         Route::delete("v1/articles/{category}", [ArticleCategoryController::class, 'delete']);
@@ -31,7 +33,7 @@ Route::middleware([
     });
 
     Route::get('v1/auth/user', [AuthController::class, 'userInfo']);
-    Route::middleware(["can:admin,all"])->group(function() {
+    Route::middleware(["can:admin,all"])->group(function () {
         Route::put('v1/pages/{category}/{page}', [PageController::class, 'update']);
         Route::put('v1/pages/{page:id}', [PageController::class, 'update']);
         Route::post('v1/pages/{category}', [PageController::class, 'create']);
@@ -45,10 +47,28 @@ Route::middleware([
 
 Route::post('v1/auth/logout', [AuthController::class, 'logout']);
 Route::post('v1/auth/refresh', [AuthController::class, 'refresh']);
-Route::post('v1/auth/login', [AuthController::class, 'login']);
-Route::post('v1/auth/register-user', [AuthController::class, 'register']);
-Route::post('v1/auth/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('v1/auth/reset-password/{token}', [AuthController::class, 'resetPassword']);
+Route::middleware(['middleware' => 'throttle:3,10'])->group(function () {
+    Route::post('v1/auth/forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('v1/auth/login', [AuthController::class, 'login']);
+    Route::post('v1/auth/register-user', [AuthController::class, 'register']);
+    Route::post('v1/contact', function (Request $request) {
+        $data = $request->validate([
+            "name" => "required|min:3",
+            "subject" => "required|min:3",
+            "email" => "required|email",
+            "message" => "required|min:10"
+        ]);
+        Mail::raw($data["message"], function($mail) use ($data) {
+            $name = $data['name'];
+            $subject = $data['subject'];
+            $mail->from($data["email"])->to(env('CONTACT_MAIL'))->subject("Contact form mail from $name with subject: $subject");
+        });
+
+        return response()->json([]);
+    });
+});
+
 
 Route::get('v1/pages', [PageCategoryController::class, 'list']);
 Route::get('v1/pages/{category}', [PageController::class, 'list']);
